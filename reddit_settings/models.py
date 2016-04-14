573e680f676a90306cb5490de189f4bbf0a12120
ben <crawfordbt@appstate.edu>
2016-04-14 16:41:38 -0400

    Removed subreddits

--- reddit_settings/models.py
+++ reddit_settings/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
-
-class RedditSetting(models.Model):
+class Subreddit(models.Model):
+    name = models.CharField()
     url = models.URLField()
+
+
+class UserSetting(models.Model):
+    subreddit = models.ForeignKey(
+        'Subreddit',
+        on_delete=models.CASCADE,
+    )
+    social_account = models.ForeignKey(
+        'SocialAccount',
+        on_delete=models.CASCADE,
+    )
+    frequency = models.IntegerField()
--- subreddits/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubredditsConfig(AppConfig):
-    name = 'subreddits'
